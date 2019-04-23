0c6a944255f7af370dbb7ef9cab83f9ebde56831
AlexhahahaDrag <734663446@qq.com>
2019-04-23 13:46:29 +0800

    find smallest letter greater than target

---
+++ src/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,57 @@
+/**
+ * @Description: Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
+ *
+ * Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+ *
+ * Examples:
+ * Input:
+ * letters = ["c", "f", "j"]
+ * target = "a"
+ * Output: "c"
+ *
+ * Input:
+ * letters = ["c", "f", "j"]
+ * target = "c"
+ * Output: "f"
+ *
+ * Input:
+ * letters = ["c", "f", "j"]
+ * target = "d"
+ * Output: "f"
+ *
+ * Input:
+ * letters = ["c", "f", "j"]
+ * target = "g"
+ * Output: "j"
+ *
+ * Input:
+ * letters = ["c", "f", "j"]
+ * target = "j"
+ * Output: "c"
+ *
+ * Input:
+ * letters = ["c", "f", "j"]
+ * target = "k"
+ * Output: "c"
+ * Note:
+ * letters has a length in range [2, 10000].
+ * letters consists of lowercase letters, and contains at least 2 unique letters.
+ * target is a lowercase letter.
+ * @Author:      alex
+ * @CreateDate:  2019/4/23 13:45
+ * @param
+ * @return
+*/
+public class FindSmallestLetterGreaterThanTarget {
+    public static void main(String[] args) {
+        char[] letters={'c','f','j'};
+        System.out.println(nextGreatestLetter(letters,'a'));
+    }
+    public static char nextGreatestLetter(char[] letters, char target) {
+        for(char ch:letters){
+            if(ch>target)
+                return ch;
+        }
+        return letters[0];
+    }
+}
