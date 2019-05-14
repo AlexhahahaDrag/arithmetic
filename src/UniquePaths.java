81967230dd3e40666e18108e8cf5632a8b1a2417
AlexhahahaDrag <734663446@qq.com>
2019-05-14 11:01:13 +0800

    lowest common ancestor of a binary search tree unique paths

---
+++ src/UniquePaths.java
@@ -0,0 +1,58 @@
+/**
+ * @Description: A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ *
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ *
+ * How many possible unique paths are there?
+ *
+ *
+ * Above is a 7 x 3 grid. How many possible unique paths are there?
+ *
+ * Note: m and n will be at most 100.
+ *
+ * Example 1:
+ *
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation:
+ * From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down
+ * 2. Right -> Down -> Right
+ * 3. Down -> Right -> Right
+ * Example 2:
+ *
+ * Input: m = 7, n = 3
+ * Output: 28
+ * @Author:      alex
+ * @CreateDate:  2019/5/13 17:40
+ * @param
+ * @return
+*/
+public class UniquePaths {
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(7,3));
+    }
+    public static int uniquePaths(int m, int n) {
+        if(m==1&&n==1)
+            return 1;
+        else if((m==1&&n==2)||(m==2&&n==1))
+            return 1;
+        else if(n==1)
+            return uniquePaths(m-1,n);
+        else if(m==1)
+            return uniquePaths(m,n-1);
+        else
+            return uniquePaths(m-1,n)+uniquePaths(m,n-1);
+    }
+    public static int uniquePaths2(int m, int n) {
+        Integer[][] map = new Integer[m][n];
+        for(int i = 0; i<m;i++)
+            map[i][0] = 1;
+        for(int j= 1;j<n;j++)
+            map[0][j]=1;
+        for(int i = 1;i<m;i++)
+            for(int j = 1;j<n;j++)
+                map[i][j] = map[i-1][j]+map[i][j-1];
+        return map[m-1][n-1];
+    }
+}
---
+++ src/UniquePathsII.java
@@ -0,0 +1,56 @@
+/**
+ * @Description: A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ *
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ *
+ * Now consider if some obstacles are added to the grids. How many unique paths would there be?
+ *
+ *
+ *
+ * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ *
+ * Note: m and n will be at most 100.
+ *
+ * Example 1:
+ *
+ * Input:
+ * {
+ *   {0,0,0},
+ *   {0,1,0},
+ *   {0,0,0}
+ * }
+ * Output: 2
+ * Explanation:
+ * There is one obstacle in the middle of the 3x3 grid above.
+ * There are two ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down -> Down
+ * 2. Down -> Down -> Right -> Right
+ * @Author:      alex
+ * @CreateDate:  2019/5/14 10:30
+ * @param
+ * @return
+ */
+public class UniquePathsII {
+    public static void main(String[] args) {
+        int[][] obstacleGrid={{1,0}};
+        System.out.println(uniquePathsWithObstacles(obstacleGrid));
+    }
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] map = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for(int i=0;i<obstacleGrid.length;i++){
+            if (obstacleGrid[i][0]==1)
+                break;
+            map[i][0]=1;
+        }
+        for(int j=0;j<obstacleGrid[0].length;j++){
+            if (obstacleGrid[0][j]==1)
+                break;
+            map[0][j]=1;
+        }
+        for(int k=1;k<obstacleGrid.length;k++)
+            for(int l=1;l<obstacleGrid[k].length;l++)
+                if (obstacleGrid[k][l]!=1)
+                    map[k][l]=map[k-1][l]+map[k][l-1];
+        return map[obstacleGrid.length-1][obstacleGrid[0].length-1];
+    }
+}
