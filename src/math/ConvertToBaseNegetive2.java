8a4691fb95d3321f492af67f3157d70346814353
AlexhahahaDrag <734663446@qq.com>
2019-07-18 17:50:21 +0800

    convert to base -2

---
+++ src/math/ConvertToBaseNegetive2.java
@@ -0,0 +1,39 @@
+package math;
+
+/**
+ * @Description: Given a number N, return a string consisting of "0"s and "1"s that represents its value in base -2 (negative two).
+ *
+ * The returned string must have no leading zeroes, unless the string is "0".
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: 2
+ * Output: "110"
+ * Explantion: (-2) ^ 2 + (-2) ^ 1 = 2
+ * Example 2:
+ *
+ * Input: 3
+ * Output: "111"
+ * Explantion: (-2) ^ 2 + (-2) ^ 1 + (-2) ^ 0 = 3
+ * Example 3:
+ *
+ * Input: 4
+ * Output: "100"
+ * Explantion: (-2) ^ 2 = 4
+ * @Author:     alex
+ * @CreateDate: 2019/7/18 17:48
+ * @Version:    1.0
+ *
+*/
+public class ConvertToBaseNegetive2 {
+    public static void main(String[] args) {
+        ConvertToBaseNegetive2 convertToBaseNegetive2 = new ConvertToBaseNegetive2();
+        System.out.println(convertToBaseNegetive2.baseNeg2(47));
+    }
+    public String baseNeg2(int N) {
+        //1431655765  (2^2+1)(2^4+1)(2^8+1)(2^16+1)的化简结果
+        return Integer.toBinaryString(1431655765 ^ (1431655765 - N));
+    }
+}
