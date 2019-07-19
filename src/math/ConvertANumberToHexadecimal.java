381c2af29300ffd53eccbf7c1c35260c8eeb7d25
AlexhahahaDrag <734663446@qq.com>
2019-07-19 11:52:13 +0800

    update method convert a number to hexadecimal

--- src/math/ConvertANumberToHexadecimal.java
+++ src/math/ConvertANumberToHexadecimal.java
@@ -27,7 +27,7 @@ package math;
  * @CreateDate: 2019/7/19 11:25
  * @Version:    1.0
  *
-*/
+ */
 public class ConvertANumberToHexadecimal {
     public static void main(String[] args) {
         ConvertANumberToHexadecimal convertANumberToHexadecimal = new ConvertANumberToHexadecimal();
@@ -35,40 +35,44 @@ public class ConvertANumberToHexadecimal {
         System.out.println(0b11);
         System.out.println(031);
         System.out.println(0xa7);
+        System.out.println(convertANumberToHexadecimal.toHex(-1));
         System.out.println(convertANumberToHexadecimal.toHex1(-1));
+        System.out.println(convertANumberToHexadecimal.toHex2(-1));
     }
     public String toHex2(int num) {
         StringBuffer res = new StringBuffer();
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
         for (int i=0; i<8; i++) {
             int number = num & (0b1111);
             num = num >>> 4;
-            res.insert(0, ch[number]);
+            res.insert(0, map(number));
         }
         while (res.charAt(0)=='0' && res.length()>1) res.deleteCharAt(0);
         return res.toString();
     }
     public String toHex1(int num) {
         StringBuffer res = new StringBuffer();
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-       while(num != 0) {
+        while(num != 0) {
             int number = num & (0b1111);
             num = num >>> 4;
-            res.insert(0, ch[number]);
+            res.insert(0, map(number));
         }
         while (res.charAt(0)=='0' && res.length()>1) res.deleteCharAt(0);
         return res.toString();
     }
     public String toHex(int num) {
         if (num == 0) return "0";
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
         final int MASK_LAST_FOUR_BITS = 0x00_00_00_0f; // = 0b1111
         final StringBuilder sb = new StringBuilder();
         while (num != 0) {
             int lastFourBits = num & MASK_LAST_FOUR_BITS;
-            sb.append(ch[lastFourBits]);
+            sb.append(map(lastFourBits));
             num = num >>> 4; // I don't know whether It can be written like num >>>= 4; ? ..emm
         }
         return sb.reverse().toString();
+    }
+    private char map (int num) {
+        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        return ch[num];
     }
 }
