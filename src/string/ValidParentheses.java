f70f75e2ced407a9fe9e4e7b01a595b952ed1802
AlexhahahaDrag <734663446@qq.com>
2019-10-21 11:45:52 +0800

    valid parenthesis rotate image

---
+++ src/string/ValidParentheses.java
@@ -0,0 +1,102 @@
+package string;
+
+import java.util.Stack;
+
+/**
+ * @Description: 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author:     alex
+ * @CreateDate: 2019/10/21 10:30
+ * @Version:    1.0
+ *
+ */
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        ValidParentheses validParentheses = new ValidParentheses();
+        System.out.println(validParentheses.isValid("([)]"));
+    }
+
+    public boolean isValid1(String s) {
+        char ch[] = s.toCharArray();
+        Stack<Character> track = new Stack<>();
+        for(char c : ch) {
+            if(c == '{' || c == '[' || c == '(')
+                track.add(c);
+            else {
+                if (track.size() == 0)
+                    return false;
+                char chSt = track.pop();
+                if (!map(chSt, c))
+                    return false;
+            }
+        }
+        if(track.size() > 0)
+            return false;
+        return true;
+    }
+
+    private boolean map(char ch1, char ch2) {
+        if((ch1 == '(' && ch2 == ')') || (ch1 == '{' && ch2 == '}') || (ch1 == '[' && ch2 == ']'))
+            return true;
+        return false;
+    }
+
+    public boolean isValid(String s) {
+        char ch[] = s.toCharArray();
+        char[] stack = new char[ch.length];
+        int len = 0;
+        for(char c : ch) {
+            switch (c) {
+                case '{':
+                case '[':
+                case '(':
+                    stack[len++] = c;
+                    break;
+                case ')':
+                    if (len == 0 || stack[--len] != '(')
+                        return false;
+                    break;
+                case ']':
+                    if (len == 0 || stack[--len] != '[')
+                        return false;
+                    break;
+                case '}':
+                    if (len == 0 || stack[--len] != '{')
+                        return false;
+                    break;
+            }
+        }
+        return len == 0;
+    }
+}
---
+++ src/twoDimensionalArray/RotateImage.java
@@ -0,0 +1,76 @@
+package twoDimensionalArray;
+
+/**
+ * @Description: 给定一个 n × n 的二维矩阵表示一个图像。
+ *
+ * 将图像顺时针旋转 90 度。
+ *
+ * 说明：
+ *
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ *
+ * 示例 1:
+ *
+ * 给定 matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ * 示例 2:
+ *
+ * 给定 matrix =
+ * [
+ *   [ 5, 1, 9,11],
+ *   [ 2, 4, 8,10],
+ *   [13, 3, 6, 7],
+ *   [15,14,12,16]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [15,13, 2, 5],
+ *   [14, 3, 4, 1],
+ *   [12, 6, 8, 9],
+ *   [16, 7,10,11]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-image
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author:     alex
+ * @CreateDate: 2019/10/21 11:05
+ * @Version:    1.0
+ *
+*/
+public class RotateImage {
+
+    public static void main(String[] args) {
+        RotateImage rotateImage = new RotateImage();
+        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+        rotateImage.rotate(matrix);
+        System.out.println(1);
+    }
+
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        boolean[][] map = new boolean[len][len];
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < len; j++) {
+                if(!map[i][j]) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][len - 1 - i];
+                    matrix[j][len - 1 - i] = temp;
+                    map[j][len - 1 - i] = true;
+                }
+            }
+        }
+    }
+}
