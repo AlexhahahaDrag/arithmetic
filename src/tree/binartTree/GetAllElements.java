2ff162dea6e021c15f7826df80ff7ad8f739508e
alexhahahaDrag <734663446@qq.com>
2019-12-29 15:54:58 +0800

    get all elements in two trees; is symmetric tree

---
+++ src/tree/binartTree/GetAllElements.java
@@ -0,0 +1,85 @@
+package tree.binartTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *description:  
+ *author:       alex
+ *createDate:  2019/12/29 11:40
+ *version:      1.0.0      
+*/
+public class GetAllElements {
+
+    List<Integer> res = new ArrayList<>();
+    List<Integer> remain = new ArrayList<>();
+    int remainInd = 0;
+    public List<Integer> getAllElements1(TreeNode root1, TreeNode root2) {
+        getList(root1, root2);
+        for(int i = remainInd; i < remain.size(); i++)
+            res.add(remain.get(remainInd));
+        return res;
+    }
+    private void getList(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null)
+            ;
+        else if(root1 == null) {
+            getList(root1, root2.left);
+            remain.add(root2.val);
+            getList(root1, root2.right);
+        } else if (root2 == null) {
+            getList(root1.left, root2);
+            remain.add(root1.val);
+            getList(root1.right, root2);
+        } else {
+            getList(root1.left, root2.left);
+            add(root1.val, root2.val);
+            getList(root1.right, root2.right);
+        }
+    }
+    private void add(int val1, int val2) {
+        int val = Math.min(val1, val2);
+        remain.add(Math.max(val1, val2));
+        while(remain.get(remainInd) < val)
+            res.add(remain.get(remainInd++));
+        res.add(val);
+    }
+
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> res = new ArrayList<>();
+        List<Integer> list1  = new ArrayList<>();
+        getL(root1, list1);
+        List<Integer> list2 = new ArrayList<>();
+        getL(root2, list2);
+        int index1 = 0;
+        int index2 = 0;
+        int len1 = list1.size();
+        int len2 = list2.size();
+        while(index1 < len1 && index2 < len2) {
+            if(list1.get(index1) < list2.get(index2))
+                res.add(list1.get(index1++));
+             else
+                res.add(list2.get(index2++));
+        }
+        while(index1 < len1)
+            res.add(list1.get(index1++));
+        while(index2 < len2)
+            res.add(list2.get(index2++));
+        return res;
+    }
+
+    private void getL(TreeNode treeNode, List<Integer> list) {
+        if (treeNode == null)
+            return;
+        getL(treeNode.left, list);
+        list.add(treeNode.val);
+        getL(treeNode.right, list);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,1,7,0,2};
+        TreeNode treeNode = TreeNode.getTreeNodeByArr(arr);
+        GetAllElements getAllElements = new GetAllElements();
+        System.out.println(getAllElements.getAllElements(null, treeNode));
+    }
+}
---
+++ src/tree/binartTree/IsSymmetric.java
@@ -0,0 +1,47 @@
+package tree.binartTree;
+
+/**
+ *description:
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *author:       alex
+ *createDate:  2019/12/29 12:16
+ *version:      1.0.0      
+*/
+public class IsSymmetric {
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+            return true;
+        return is(root.left, root.right);
+    }
+
+    private boolean is(TreeNode root1, TreeNode root2) {
+        if(root1 != null && root2 != null) {
+            if(root1.val != root2.val)
+                return false;
+            return is(root1.left, root2.right) && is(root1.right, root2.left);
+        } else if(root1 == null && root2 == null)
+            return true;
+        else
+            return false;
+    }
+}
