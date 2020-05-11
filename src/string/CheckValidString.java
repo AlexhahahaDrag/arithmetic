c660114e25d04faf1e321575d525e87f78bdb696
majf <734663446@qq.com>
2020-05-11 22:40:51 +0800

    check valid string max Score score of parentheses

---
+++ src/string/CheckValidString.java
@@ -0,0 +1,71 @@
+package string;
+
+/**
+ * Description:
+ * 678. 有效的括号字符串
+ * 给定一个只包含三种字符的字符串：（ ，） 和 *，写一个函数来检验这个字符串是否为有效字符串。有效字符串具有如下规则：
+ *
+ * 任何左括号 ( 必须有相应的右括号 )。
+ * 任何右括号 ) 必须有相应的左括号 ( 。
+ * 左括号 ( 必须在对应的右括号之前 )。
+ * * 可以被视为单个右括号 ) ，或单个左括号 ( ，或一个空字符串。
+ * 一个空字符串也被视为有效字符串。
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: True
+ * 示例 2:
+ *
+ * 输入: "(*)"
+ * 输出: True
+ * 示例 3:
+ *
+ * 输入: "(*))"
+ * 输出: True
+ * 注意:
+ *
+ * 字符串大小将在 [1，100] 范围内。
+ * Author:      alex
+ * CreateDate:  2020/5/11 22:12
+ * Version:     1.0
+ *
+*/
+public class CheckValidString {
+
+    public boolean checkValidString(String s) {
+        int left = 0;
+        int len = s.length();
+        int index = 0;
+        while(index < len) {
+            char ch = s.charAt(index++);
+            if (ch == '(' || ch == '*')
+                left++;
+            else if (ch == ')') {
+                if (left > 0)
+                    left--;
+                else
+                    return false;
+            }
+        }
+        int right = 0;
+        index = len - 1;
+        while(index >= 0) {
+            char ch = s.charAt(index--);
+            if (ch == '*' || ch == ')')
+                right++;
+            else if (ch == '(') {
+                if (right > 0)
+                    right--;
+                else
+                    return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        CheckValidString checkValidString = new CheckValidString();
+        System.out.println(checkValidString.checkValidString("(((((*)))"));
+    }
+}
---
+++ src/string/ScoreOfParentheses.java
@@ -0,0 +1,77 @@
+package string;
+
+import java.util.Stack;
+
+/**
+ * Description:
+ * 856. 括号的分数
+ * 给定一个平衡括号字符串 S，按下述规则计算该字符串的分数：
+ *
+ * () 得 1 分。
+ * AB 得 A + B 分，其中 A 和 B 是平衡括号字符串。
+ * (A) 得 2 * A 分，其中 A 是平衡括号字符串。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入： "()"
+ * 输出： 1
+ * 示例 2：
+ *
+ * 输入： "(())"
+ * 输出： 2
+ * 示例 3：
+ *
+ * 输入： "()()"
+ * 输出： 2
+ * 示例 4：
+ *
+ * 输入： "(()(()))"
+ * 输出： 6
+ *
+ *
+ * 提示：
+ *
+ * S 是平衡括号字符串，且只含有 ( 和 ) 。
+ * 2 <= S.length <= 50
+ * Author:      alex
+ * CreateDate:  2020/5/11 21:54
+ * Version:     1.0
+ *
+*/
+public class ScoreOfParentheses {
+
+    public int scoreOfParentheses(String S) {
+        int index = 0;
+        int len = S.length();
+        Stack<Integer> stack = new Stack();
+        while (index < len) {
+            if (S.charAt(index) == '(')
+                stack.add(-1);
+            else if (!stack.isEmpty()){
+                int sum = 0;
+                Integer object = stack.pop();
+                while(!stack.isEmpty() && object != -1) {
+                    sum += object;
+                    object = stack.pop();
+                }
+                if (object == -1) {
+                    sum = sum == 0 ? 1 : sum << 1;
+                    stack.add(sum);
+                } else
+                    stack.add(sum);
+            }
+            index++;
+        }
+        int sum = 0;
+        while(!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ScoreOfParentheses scoreOfParentheses = new ScoreOfParentheses();
+        System.out.println(scoreOfParentheses.scoreOfParentheses("()"));
+    }
+}
