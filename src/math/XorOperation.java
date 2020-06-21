651472271d5ac6e02095d8b7207f751a3ec23886
alexhahahaDrag <734663446@qq.com>
2020-06-21 14:28:21 +0800

    contest 194; xor operation; get folder names; avoid flood;

---
+++ src/math/XorOperation.java
@@ -0,0 +1,63 @@
+package math;
+
+/**
+ *description:
+ * 5440. 数组异或操作
+ *  显示英文描述
+ * 我的提交
+ * 返回竞赛
+ *
+ * 通过的用户数
+ * 2836
+ * 尝试过的用户数
+ * 2855
+ * 用户总通过次数
+ * 2882
+ * 用户总提交次数
+ * 3092
+ * 题目难度
+ * Easy
+ * 给你两个整数，n 和 start 。
+ * 数组 nums 定义为：nums[i] = start + 2*i（下标从 0 开始）且 n == nums.length 。
+ * 请返回 nums 中所有元素按位异或（XOR）后得到的结果。
+ *
+ * 示例 1：
+ * 输入：n = 5, start = 0
+ * 输出：8
+ * 解释：数组 nums 为 [0, 2, 4, 6, 8]，其中 (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8 。
+ *      "^" 为按位异或 XOR 运算符。
+ * 示例 2：
+ * 输入：n = 4, start = 3
+ * 输出：8
+ * 解释：数组 nums 为 [3, 5, 7, 9]，其中 (3 ^ 5 ^ 7 ^ 9) = 8.
+ * 示例 3：
+ * 输入：n = 1, start = 7
+ * 输出：7
+ * 示例 4：
+ * 输入：n = 10, start = 5
+ * 输出：2
+ *
+ * 提示：
+ * 1 <= n <= 1000
+ * 0 <= start <= 1000
+ * n == nums.length
+ *author:       alex
+ *createDate:   2020/6/9 20:42
+ *version:      1.0.0
+ */
+public class XorOperation {
+
+    public int xorOperation(int n, int start) {
+        int res = 0;
+        while(n-- > 0) {
+            res ^= start;
+            start += 2;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        XorOperation xorOperation = new XorOperation();
+        System.out.println(xorOperation.xorOperation(10, 5));
+    }
+}
