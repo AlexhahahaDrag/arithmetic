af3e70134545b962261621b493fffd0891866c20
majf <734663446@qq.com>
2020-07-17 13:13:36 +0800

    search insert; sub sort;

---
+++ src/array/SearchInsert.java
@@ -0,0 +1,72 @@
+package array;
+
+/**
+ * @description:
+ * 35. 搜索插入位置
+ * 难度
+ * 简单
+ *
+ * 587
+ *
+ * 收藏
+ *
+ * 分享
+ *
+ * 切换为英文
+ *
+ * 关注
+ *
+ * 反馈
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 你可以假设数组中无重复元素。
+ * 示例 1:
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 示例 2:
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 示例 3:
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 示例 4:
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ * @author:      alex
+ * @createTime:  2020/7/17 12:22
+ * @version:     1.0
+ */
+public class SearchInsert {
+
+    public int searchInsert(int[] nums, int target) {
+        if (target < nums[0])
+            return 0;
+        int len = nums.length;
+        if (target > nums[len - 1])
+            return len;
+        int left = 0;
+        int right = len - 1;
+        int mid = 0;
+        while(left < right) {
+            mid = left + (right - left >> 1);
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target) {
+                if (nums[mid + 1] >= target)
+                    return mid + 1;
+                left = mid + 1;
+            } else {
+                if (nums[mid - 1] < target)
+                    return mid;
+                right = mid;
+            }
+        }
+        return mid;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3};
+        int target = 2;
+        SearchInsert searchInsert = new SearchInsert();
+        System.out.println(searchInsert.searchInsert(nums, target));
+    }
+}
---
+++ src/array/SubSort.java
@@ -0,0 +1,81 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * @description:
+ * 面试题 16.16. 部分排序
+ * 难度
+ * 中等
+ *
+ * 25
+ *
+ * 收藏
+ *
+ * 分享
+ *
+ * 切换为英文
+ *
+ * 关注
+ *
+ * 反馈
+ * 给定一个整数数组，编写一个函数，找出索引m和n，只要将索引区间[m,n]的元素排好序，整个数组就是有序的。注意：n-m尽量最小，也就是说，找出符合条件的最短序列。函数返回值为[m,n]，若不存在这样的m和n（例如整个数组是有序的），请返回[-1,-1]。
+ * 示例：
+ * 输入： [1,2,4,7,10,11,7,12,6,7,16,18,19]
+ * 输出： [3,9]
+ * 提示：
+ * 0 <= len(array) <= 1000000
+ * @author:      alex
+ * @createTime:  2020/7/17 12:39
+ * @version:     1.0
+ */
+public class SubSort {
+
+    public int[] subSort1(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+        int[] ne = Arrays.copyOf(array, array.length);
+        Arrays.sort(ne);
+        while(start < end && ne[start] == array[start]) {
+            start++;
+        }
+        while(start < end && ne[end] == array[end]) {
+            end--;
+        }
+        if (start >= end)
+            return new int[]{-1, -1};
+        return new int[] {start, end};
+    }
+
+    public int[] subSort(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+        while(start < array.length - 1 && array[start] <= array[start + 1])
+            start++;
+        while(start < end && array[end] >= array[end - 1])
+            end--;
+        if (start >= end)
+            return new int[]{-1, -1};
+        int min = array[start];
+        int max = array[end];
+        for (int i = start + 1; i <= end; i++) {
+            if (min > array[i])
+                min = array[i];
+            if (max < array[i])
+                max = array[i];
+        }
+        while(start >= 0 && min < array[start])
+            start--;
+        while(end < array.length && max > array[end])
+            end++;
+        return new int[] {start + 1, end - 1};
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,2,4,7,10,11,7,12,6,7,16,18,19};
+        SubSort subSort = new SubSort();
+        int[] res = subSort.subSort(array);
+        for (int i : res)
+            System.out.println(i +  "  ");
+    }
+}
