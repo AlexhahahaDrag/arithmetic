86ab5a6c1c6736f39dccfa71c02a9dd8d289c3a0
majf <734663446@qq.com>
2020-08-25 20:35:34 +0800

    find subsequences

---
+++ src/array/FindSubsequences.java
@@ -0,0 +1,36 @@
+package array;
+
+import java.util.List;
+
+/**
+ * @description:
+ * 491. 递增子序列
+ * 给定一个整型数组, 你的任务是找到所有该数组的递增子序列，递增子序列的长度至少是2。
+ *
+ * 示例:
+ *
+ * 输入: [4, 6, 7, 7]
+ * 输出: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
+ * 说明:
+ *
+ * 给定数组的长度不会超过15。
+ * 数组中的整数范围是 [-100,100]。
+ * 给定数组中可能包含重复数字，相等的数字应该被视为递增的一种情况。
+ * @author:      alex
+ * @createTime:  2020/8/25 20:28
+ * @version:     1.0
+ */
+public class FindSubsequences {
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+
+    }
+
+    private void
+
+    public static void main(String[] args) {
+        int[] nums = {4, 6, 7, 7};
+        FindSubsequences findSubsequences = new FindSubsequences();
+        System.out.println(findSubsequences.findSubsequences(nums));
+    }
+}
