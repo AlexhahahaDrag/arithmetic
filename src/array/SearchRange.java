0ca118722c40eaf2b638a830d79d999f31358e59
majf <734663446@qq.com>
2020-12-08 20:39:51 +0800

    split into fibonacci

--- src/array/SearchRange.java
+++ src/array/SearchRange.java
@@ -26,44 +26,7 @@ import java.util.Arrays;
 */
 public class SearchRange {
 
-    public int[] searchRange1(int[] nums, int target) {
-        int[] result = new int[2];
-        Arrays.fill(result, -1);
-        int start = 0;
-        int end = nums.length - 1;
-        while(start <= end) {
-            int mid = start + (end - start >> 1);
-            if(target < nums[mid])
-                end = mid - 1;
-            else if (target > nums[mid])
-                start = mid + 1;
-            else {
-                result[0] = find(nums, start, mid, target);
-                result[1] = find(nums, mid, end, target);
-                break;
-            }
-        }
-        return result;
-    }
-
-    private int find1(int[] nums, int start, int end, int target) {
-        while(start <= end) {
-            int mid = start + (end - start >> 1);
-            if(target < nums[mid])
-                end = mid - 1;
-            else if (target > nums[mid])
-                start = mid + 1;
-            else {
-                if (nums[mid] == nums[end])
-                    end = mid - 1;
-                else
-                    start = mid + 1;
-            }
-        }
-        return start;
-    }
-
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange2(int[] nums, int target) {
         if (nums == null || nums.length < 1)
             return new int[2];
         int num = 0;
@@ -96,10 +59,56 @@ public class SearchRange {
         return -1;
     }
 
+//    public int[] searchRange(int[] nums, int target) {
+//        int index = binary(nums, 0, nums.length - 1, target);
+//        if (index == -1)
+//            return new int[] {-1, -1};
+//        int start = index; int end = index;
+//        while(start - 1 >= 0 && nums[start - 1] == target)
+//            start--;
+//        while(end + 1 < nums.length && nums[end + 1] == target)
+//            end++;
+//        return new int[] {start, end};
+//    }
+//
+//    private int binary(int[] nums, int start, int end, int target) {
+//        while(start <= end) {
+//            int mid = start + (end - start >> 1);
+//            if(nums[mid] > target)
+//                end = mid - 1;
+//            else if (nums[mid] < target)
+//                start = mid + 1;
+//            else
+//                return mid;
+//        }
+//        return -1;
+//    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int start = binary(nums, 0, nums.length - 1, target, true);
+        int end = binary(nums, 0, nums.length - 1, target, false) - 1;
+        if (start <= end && end < nums.length && nums[start] == target && nums[end] == target)
+            return new int[] {start, end};
+        return new int[] {-1, -1};
+    }
+
+    private int binary(int[] nums, int start, int end, int target, boolean low) {
+        int ans = nums.length;
+        while(start <= end) {
+            int mid = start + (end - start >> 1);
+            if(nums[mid] > target || (low && target <= nums[mid])) {
+                end = mid - 1;
+                ans = mid;
+            } else
+                start = mid + 1;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         SearchRange searchRange = new SearchRange();
-        int target = 8;
-        int nums[] = {5, 7, 7, 8, 8, 10};
+        int target = 2;
+        int nums[] = { 2};
         int[] arr = searchRange.searchRange(nums, target);
         for(int i : arr)
             System.out.print(i + " ");
---
+++ src/math/SplitIntoFibonacci.java
@@ -0,0 +1,99 @@
+package math;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @description:
+ * 842. 将数组拆分成斐波那契序列
+ * 给定一个数字字符串 S，比如 S = "123456579"，我们可以将它分成斐波那契式的序列 [123, 456, 579]。
+ *
+ * 形式上，斐波那契式序列是一个非负整数列表 F，且满足：
+ *
+ * 0 <= F[i] <= 2^31 - 1，（也就是说，每个整数都符合 32 位有符号整数类型）；
+ * F.length >= 3；
+ * 对于所有的0 <= i < F.length - 2，都有 F[i] + F[i+1] = F[i+2] 成立。
+ * 另外，请注意，将字符串拆分成小块时，每个块的数字一定不要以零开头，除非这个块是数字 0 本身。
+ *
+ * 返回从 S 拆分出来的任意一组斐波那契式的序列块，如果不能拆分则返回 []。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入："123456579"
+ * 输出：[123,456,579]
+ * 示例 2：
+ *
+ * 输入: "11235813"
+ * 输出: [1,1,2,3,5,8,13]
+ * 示例 3：
+ *
+ * 输入: "112358130"
+ * 输出: []
+ * 解释: 这项任务无法完成。
+ * 示例 4：
+ *
+ * 输入："0123"
+ * 输出：[]
+ * 解释：每个块的数字不能以零开头，因此 "01"，"2"，"3" 不是有效答案。
+ * 示例 5：
+ *
+ * 输入: "1101111"
+ * 输出: [110, 1, 111]
+ * 解释: 输出 [11,0,11,11] 也同样被接受。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= S.length <= 200
+ * 字符串 S 中只含有数字。
+ * @author:      alex
+ * @createTime:  2020/12/8 20:05
+ * @version:     1.0
+ */
+public class SplitIntoFibonacci {
+
+    public List<Integer> splitIntoFibonacci(String S) {
+        int[] res = new int[S.length()];
+        find(res, 0, S.toCharArray(), 0);
+        List<Integer> list = new ArrayList<>();
+        for(int i : res)
+            list.add(i);
+        return list;
+    }
+
+    private boolean find(int[] res, int index, char[] chs, int in) {
+        if (!check(res, index)) {
+            res[index - 1] = 0;
+            return false;
+        }
+
+        if (in == chs.length)
+            return true;
+        if (in > chs.length || chs[in] == '0') {
+            res[index - 1] = 0;
+            return false;
+        }
+        for (int i = in; i < chs.length; i++) {
+            res[index] = res[index] * 10 + chs[i] - '0';
+            if (find(res, index + 1, chs, i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(int[] res, int index) {
+        if (index < 3)
+            return true;
+        return res[index] - res[index - 1] - res[index - 2] == 0;
+    }
+
+    public static void main(String[] args) {
+        String S = "1101111";
+        SplitIntoFibonacci splitIntoFibonacci = new SplitIntoFibonacci();
+        System.out.println(splitIntoFibonacci.splitIntoFibonacci(S));
+    }
+}
