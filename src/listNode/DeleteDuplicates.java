6b488387be936755139d1d6a89f2190f810e357c
majf <734663446@qq.com>
2021-03-26 12:31:57 +0800

    Delete Duplicates listnode

--- src/listNode/DeleteDuplicates.java
+++ src/listNode/DeleteDuplicates.java
@@ -2,8 +2,8 @@ package listNode;
 
 /**
  * @description:
- * 82. 删除排序链表中的重复元素 II
- * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+ * 83. 删除排序链表中的重复元素
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
  *
  * 返回同样按升序排列的结果链表。
  *
@@ -12,13 +12,13 @@ package listNode;
  * 示例 1：
  *
  *
- * 输入：head = [1,2,3,3,4,4,5]
- * 输出：[1,2,5]
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
  * 示例 2：
  *
  *
- * 输入：head = [1,1,1,2,3]
- * 输出：[2,3]
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
  *
  *
  * 提示：
@@ -27,35 +27,26 @@ package listNode;
  * -100 <= Node.val <= 100
  * 题目数据保证链表已经按升序排列
  * @author:      alex
- * @createTime:  2021/3/25 12:44
+ * @createTime:  2021/3/26 12:30
  * @version:     1.0
  */
 public class DeleteDuplicates {
 
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) {
-            return head;
-        }
-        ListNode pre = new ListNode(0, head);
-        ListNode cur = pre;
-        while(cur.next != null && cur.next.next != null) {
-            if (cur.next.val == cur.next.next.val) {
-                int i = cur.next.val;
-                while(cur.next != null && cur.next.val == i) {
-                    cur.next = cur.next.next;
-                }
-            } else {
-                cur = cur.next;
+        ListNode cur = head;
+        while(cur != null && cur.next != null) {
+            while (cur != null && cur.next != null && cur.val == cur.next.val) {
+                cur.next = cur.next.next;
             }
+            cur = cur.next;
         }
-        return pre.next;
+        return head;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,3,4,4,5};
+        int[] arr = {1,1,2,3,3};
         ListNode head = ListNode.getListNodeByArr(arr);
         DeleteDuplicates deleteDuplicates = new DeleteDuplicates();
-        ListNode res = deleteDuplicates.deleteDuplicates(head);
-        ListNode.print(res);
+        ListNode.print(deleteDuplicates.deleteDuplicates(head));
     }
 }
