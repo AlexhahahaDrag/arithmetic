326a5ee2c0f4784a58613caaff55a2d5121d0de9
majf <734663446@qq.com>
2021-11-03 18:03:28 +0800

    min days

--- src/string/RemoveInvalidParentheses.java
+++ src/string/RemoveInvalidParentheses.java
@@ -57,7 +57,6 @@ public class RemoveInvalidParentheses {
     }
 
     private void dfs(String s, int start, int lCount, int rCount, int lRemain, int rRemain) {
-
         if(lRemain == 0 && rRemain == 0) {
             if (judge(s)) {
                 res.add(s);
@@ -74,20 +73,20 @@ public class RemoveInvalidParentheses {
             //去除左括号
             if (s.charAt(i) == '(') {
                 if (lRemain > 0) {
-                    dfs(s.substring(0, i) + s.substring(i + 1), i + 1, lCount, rCount,lRemain - 1, rRemain);
+                    dfs(s.substring(0, i) + s.substring(i + 1), i, lCount, rCount,lRemain - 1, rRemain);
                 }
                 lCount++;
             }
             //去除右括号
             if (s.charAt(i) == ')' && rRemain > 0) {
                 if (rRemain > 0) {
-                    dfs(s.substring(0, i) + s.substring(i + 1), i + 1, lCount, rCount, lRemain, rRemain - 1);
+                    dfs(s.substring(0, i) + s.substring(i + 1), i, lCount, rCount, lRemain, rRemain - 1);
                 }
                 rCount++;
             }
-//            if (lCount < rCount) {
-//                return;
-//            }
+            if (lCount < rCount) {
+                return;
+            }
         }
     }
 
@@ -110,81 +109,6 @@ public class RemoveInvalidParentheses {
         }
         return left == 0;
     }
-
-//
-//    class Solution {
-//        private List<String> res = new ArrayList<String>();
-//
-//        public List<String> removeInvalidParentheses(String s) {
-//            int lremove = 0;
-//            int rremove = 0;
-//
-//            for (int i = 0; i < s.length(); i++) {
-//                if (s.charAt(i) == '(') {
-//                    lremove++;
-//                } else if (s.charAt(i) == ')') {
-//                    if (lremove == 0) {
-//                        rremove++;
-//                    } else {
-//                        lremove--;
-//                    }
-//                }
-//            }
-//            helper(s, 0, lremove, rremove);
-//
-//            return res;
-//        }
-//
-//        private void helper(String str, int start, int lremove, int rremove) {
-//            if (lremove == 0 && rremove == 0) {
-//                if (isValid(str)) {
-//                    res.add(str);
-//                }
-//                return;
-//            }
-//
-//            for (int i = start; i < str.length(); i++) {
-//                if (i != start && str.charAt(i) == str.charAt(i - 1)) {
-//                    continue;
-//                }
-//                // 如果剩余的字符无法满足去掉的数量要求，直接返回
-//                if (lremove + rremove > str.length() - i) {
-//                    return;
-//                }
-//                // 尝试去掉一个左括号
-//                if (lremove > 0 && str.charAt(i) == '(') {
-//                    helper(str.substring(0, i) + str.substring(i + 1), i, lremove - 1, rremove);
-//                }
-//                // 尝试去掉一个右括号
-//                if (rremove > 0 && str.charAt(i) == ')') {
-//                    helper(str.substring(0, i) + str.substring(i + 1), i, lremove, rremove - 1);
-//                }
-//            }
-//        }
-//
-//        private boolean isValid(String str) {
-//            int cnt = 0;
-//            for (int i = 0; i < str.length(); i++) {
-//                if (str.charAt(i) == '(') {
-//                    cnt++;
-//                } else if (str.charAt(i) == ')') {
-//                    cnt--;
-//                    if (cnt < 0) {
-//                        return false;
-//                    }
-//                }
-//            }
-//
-//            return cnt == 0;
-//        }
-//    }
-//
-//    作者：LeetCode-Solution
-//    链接：https://leetcode-cn.com/problems/remove-invalid-parentheses/solution/shan-chu-wu-xiao-de-gua-hao-by-leetcode-9w8au/
-//    来源：力扣（LeetCode）
-//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-
-
 
     public static void main(String[] args) {
         String s = ")(";
